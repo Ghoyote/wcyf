460876f60460d3cf1f898981ebda7adfb05155bd
Kowus <barnabas@elite-education.org>
2017-01-25 17:39:50 +0000

    defaults

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.locals.title = process.env.HEADING_WCYF;
+app.locals.title = process.env.HEADING_WCYF || 'Worldwide Christian Youth Fellowship';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
