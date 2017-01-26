fdc90f568e6f35c5aeb56e9af645004f5caf61cf
Barnabas Nomo <barnabas@elite-education.org>
2017-01-26 10:41:22 +0000

    added email model Schema, Started email API, Fixed contact form, added schema-object dependency

---
+++ models/email.model.js
@@ -0,0 +1,18 @@
+var SchemaObject = require('schema-object');
+
+var email = new SchemaObject({
+	name: String,
+	email: String,
+	category: String,
+	date: {
+		type: Date,
+		default: Date.now
+	},
+	message: String
+},{
+	keyIgnoreCase: true,
+	strict: false
+	}
+);
+
+module.exports = email;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var MailIn = require('../models/email.model');
+var date=new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+router.post('/sendmail', function (req, res, next) {
+	MailIn = req.body;
+	MailIn.date = date;
+	console.log(MailIn);
+	res.redirect('/');
+});
 module.exports = router;
