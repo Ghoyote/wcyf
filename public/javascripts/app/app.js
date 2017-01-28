419c72d9430763233b2451fa6be7656cb5a2e763
Kowus <barnabas@elite-education.org>
2017-01-28 22:30:57 +0000

    added services directive

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,33 +1,14 @@
 (function () {
-	var app = angular.module('wcyf', []).config(function ($interpolateProvider) {
+	var app = angular.module('wcyf', ['services-panel']).config(function ($interpolateProvider) {
 		$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 	});
 	
-	app.controller('ServicesController', function ()
+	/*app.controller('ServicesController', function ()
 	{
 		this.service = services;
-	})
+	});*/
 	
 	
 	
-	var services = [
-		{
-			icon: 'fa fa-4x fa-sun-o text-primary sr-icons',
-			type: "Counselling and Deliverance",
-			summary: 'Some Summary'
-		},{
-			icon: 'fa fa-4x fa-suitcase text-primary sr-icons',
-			type: "Community Outreach & Fellowship",
-			summary: 'Some Summary'
-		},{
-			icon: 'fa fa-4x fa-book text-primary sr-icons',
-			type: "Library & Store Services",
-			summary: 'Some Summary'
-		},{
-			icon: 'fa fa-4x fa-users text-primary sr-icons',
-			type: "Camp Meetings",
-			summary: 'Some Summary'
-		},
-		
-	]
+	
 })();
